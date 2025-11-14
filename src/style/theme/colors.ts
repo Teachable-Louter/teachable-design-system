65060636defdf9e130baa4d0c2366d6e9d503b58
skwo27 <165741912+skwo27@users.noreply.github.com>
2025-11-14 14:18:42 +0900

    feat :: token base

--- src/style/theme/colors.ts
+++ src/style/theme/colors.ts
@@ -9,6 +9,154 @@ export const colors = {
 
   },
   
+  surface: {
+    'white': '#FFFFFF',
+    'white-subtler': '#FFFFFF',
+    'white-subtle': '#FFFFFF',
+    'white-static': '#FFFFFF',
+    'inverse' : '#1E2124',
+    'inverse-static': '#1E2124',
+    'gray-subtler' :'#F4F5F6',
+    'gray-subtle' :'#E6E8EA',
+    'disabled' : '#CDD1D5',
+    'primary-subtler': '#ECF2FE',
+    'secondary-subtler': '#EEF2F7',
+    'point-subtler': '#FBEFF0',
+    'danger-subtler': '#FDEFEC',
+    'warning-subtler': '#FFF3DB',
+    'success-subtler': '#EAF6EC',
+    'information-subtler': '#E7F4FE'
+  },
+  
+  border: {
+    'transparency' : 'rgba(0, 0, 0, 0)',
+    'inverse' : '#FFFFFF',
+    'disabled' : '#B1B8BE',
+    'gray-light' : '#CDD1D5',
+    'gray' : '#B1B8BE',
+    'gray-dark' : '#58616A',
+    'gray-darker' : '#1E2124',
+    'primary-light' : '#D8E5FD',
+    'primary' : '#256EF4',
+    'secondary-light' : '#D6E0EB',
+    'secondary' : '#063A74',
+    'point-light' : '#F5D6D9',
+    'point' : '#D63D4A',
+    'danger-light' : '#FCDFD9',
+    'danger' : '#DE3412',
+    'warning-light' : '#FFE0A3',
+    'warning' : '#9E6A00',
+    'success-light' : '#D8EEDD',
+    'success' : '#228738',
+    'information-light' : '#D3EBFD',
+    'information' : '#0B78CB'
+  },
+  
+  divider: {
+    'gray-light' : '#CDD1D5',
+    'gray' : '#8A949E',
+    'gray-dark' : '#6D7882',
+    'gray-darker' : '#1E2124',
+    'inverse' : '#FFFFFF',
+    'primary-light' : '#D8E5FD',
+    'primary' : '#256EF4',
+    'secondary-light' : '#D6E0EB',
+    'secondary' : '#063A74',
+    'error' : '#DE3412',
+    'point' : '#D63D4A'
+  },
+  
+  text: {
+    'bolder' : '#131416',
+    'basic' : '#1E2124',
+    'subtle' : '#464C53',
+    'disabled' : '#8A949E',
+    'disabled-on' : '#6D7882',
+    'border-inverse' : '#FFFFFF',
+    'basic-inverse' : '#FFFFFF',
+    'subtle-inverse' : '#B1B8BE',
+    'static' : '#1E2124',
+    'inverse-static' : '#FFFFFF',
+    'primary' : '#0B50D0',
+    'secondary' : '#052B57',
+    'point' : '#AB2B36',
+    'danger' : '#BD2C0F',
+    'warning' : '#8A5C00',
+    'success' : '#267337',
+    'information' : '#096AB3'
+  },
+  
+  action: {
+    'white' : '#FFFFFF',
+    'primary' : 'rgba(255, 255, 255, 0)',
+    'primary-hover' : '#ECF2FE',
+    'primary-pressed' : '#D8E5FD',
+    'primary-selected' : '#ECF2FE',
+    'primary-active' : '#256EF4',
+    'secondary' : 'rgba(255, 255, 255, 0)',
+    'secondary-hover' : '#EEF2F7',
+    'secondary-pressed' : '#D6E0EB',
+    'secondary-selected' : '#EEF2F7',
+    'secondary-on' : '#rgba(255, 255, 255, 0)',
+    'secondary-on-hover' : '#FFFFFF',
+    'secondary-on-pressed' : '#D6E0EB',
+    'secondary-on-selected' : '#FFFFFF',
+    'secondary-active' : '#063A74',
+    'disabled' : '#CDD1D5'
+  },
+  
+  element: {
+    'inverse': '#FFFFFF',
+    'inverse-static': '#FFFFFF',
+    'gray-lighter': '#',
+    'gray-light': '#E6E8EA',
+    'gray': '#6D7882',
+    'gray-dark': '#58616A',
+    'disabled-light': '#CDD1D5',
+    'disabled-dark': '#8A949E',
+    'primary-light': '#D8E5FD',
+    'primary-lighter': '#ECF2FE',
+    'primary': '#256EF4',
+    'secondary-light': '#D6E0EB',
+    'secondary-lighter': '#EEF2F7',
+    'secondary': '#063A74',
+    'point-light': '#F5D6D9',
+    'point-lighter': '#FBEFF0',
+    'point': '#D63D4A',
+    'danger': '#DE3412',
+    'danger-lighter': '#FDEFEC',
+    'warning': '#FFB114',
+    'warning-lighter': '#FFF3DB',
+    'success': '#228738',
+    'success-lighter': '#EAF6EC',
+    'information': '#0B78CB',
+    'information-lighter': '#E7F4FE',
+    'logo-text' : '#575756'
+  },
+  
+  icon: {
+  
+  },
+  
+  link: {
+  
+  },
+  
+  button: {
+  
+  },
+  
+  input: {
+  
+  },
+  
+  graphic: {
+  
+  },
+  
+  alpha:{
+  
+  },
   // Grayscale
   gray: {
     50: '#FAFAFA',
@@ -30,11 +178,11 @@ export const colors = {
   info: '#2196F3',
   
   // Text Colors
-  text: {
-    primary: '#212121',
-    secondary: '#757575',
-    disabled: '#BDBDBD',
-  },
+  // text: {
+  //   primary: '#212121',
+  //   secondary: '#757575',
+  //   disabled: '#BDBDBD',
+  // },
   
 };
 
