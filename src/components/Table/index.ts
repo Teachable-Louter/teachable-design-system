476e63359d5d322a6e0ecadbaead34eae0539971
joungminsung <joungminsung080830@gmail.com>
2025-12-17 17:05:24 +0900

    remove :: instructions, Table/README.md #7

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,16 +1,2 @@
-/**
- * Table 컴포넌트
- *
- * @example
- * import Table from '@/components/Table';
- * import type { TableColumn } from '@/types/table';
- *
- * const columns: TableColumn[] = [
- *   { key: 'name', header: '이름', editable: true },
- *   { key: 'age', header: '나이', dataType: 'number' },
- * ];
- *
- * <Table columns={columns} data={data} onCellEdit={handleEdit} />
- */
 export { default } from './Table';
 export { default as Table } from './Table';
