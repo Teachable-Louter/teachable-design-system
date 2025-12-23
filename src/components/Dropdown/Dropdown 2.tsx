827eafca0a9d8fcd2a643854f439206ae814edd9
Yongjin081002 <yg4059540@gmail.com>
2025-12-23 14:49:13 +0900

    feat :: 0.3.9 input style

---
+++ src/components/Dropdown/Dropdown 2.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {DropdownProps} from "../../types/Dropdown.types";
+import {StyledBox, StyledDropDown, StyledText, StyledLabel, StyledIcon, StyledOptions, StyledOption} from "./style";
+import { arrowDownIconBase64 } from '../../assets/icons';
+
+export function Dropdown({size, options, onSelect, label, placeholder, width}: DropdownProps) {
+	const [open, setOpen] = React.useState(false);
+	const [selected, setSelected] = React.useState<string | null>(null);
+	const ref = React.useRef<HTMLDivElement>(null);
+	
+	React.useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (ref.current && !ref.current.contains(e.target as Node)) {
+				setOpen(false);
+			}
+		};
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => document.removeEventListener("mousedown", handleClickOutside);
+	}, []);
+	
+	const handleSelect = (option: string) => {
+		setSelected(option);
+		onSelect?.(option);
+		setOpen(false);
+	};
+	
+	
+	return (
+		<div style={{position: 'relative'}}>
+			<StyledLabel>{label}</StyledLabel>
+			<StyledDropDown onClick={() => setOpen((prev) => !prev)}
+				size={size}
+				width={width}
+				isOpen={open}
+				>
+				<StyledBox>
+					<StyledText
+						size={size}
+						isOpen={open}
+					>{selected ?? placeholder}</StyledText>
+					<StyledIcon size={size}>
+						<img src={arrowDownIconBase64} alt="dropdown icon" style={{width:'100%', height:'100%'}}/>
+					</StyledIcon>
+				</StyledBox>
+			</StyledDropDown>
+			{open && (
+				<StyledOptions ref={ref} size={size}>
+					{options?.map((option) => (
+						<StyledOption key={option} onClick={() => handleSelect(option)} size={size} isSelected={option === selected}>
+							{option}
+						</StyledOption>
+					))}
+				</StyledOptions>
+				)}
+		</div>
+	)
+}
