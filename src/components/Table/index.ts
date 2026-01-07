38710bbe6c3f0d7a612a9dfe97603a0048b61439
joungminsung <joungminsung080830@gmail.com>
2026-01-07 09:55:15 +0900

    fix :: TableColumn 타입 export 추가 #7

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,2 +1,10 @@
 export { default } from './Table';
 export { default as Table } from './Table';
+export type {
+  TableColumn,
+  TableProps,
+  TableRow,
+  DataType,
+  SortDirection,
+  TextAlign,
+} from '../../types/table';
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './table';
+export * from './button.types';
+export * from './checkBox.types';
+export * from './Dropdown.types';
+export * from './input.types';
+export * from './Sidebar.types';
